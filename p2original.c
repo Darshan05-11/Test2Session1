8ab3d1f5814dd3b3fc1c498eaa7c6e4e21230aed
M Govindhan <94587816+Darshan05-11@users.noreply.github.com>
2022-03-03 04:17:05 +0000

    p2

--- p2original.c
+++ p2original.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
+
 void input_triangle(float *x1,float *y1,float *x2,float *y2,float *x3,float *y3)
 {
-  printf("enter the three points");
-  scanf("%f,%f,%f,%f,%f,%f",x1,y1,x2,y2,x3,y3);  
+  printf("Enter the points :\n");
+  scanf("%f %f %f %f %f %f",x1,y1,x2,y2,x3,y3);
 }
-int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+
+float is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
 {
   int a,result;
-  a=0.5*(x1*(y2-y3)+x2(y3-y1)+x3(y1-y2));
+  a=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
   if(a==0)
   {
-    result=1;
-  }
+    result= 1;
+  } 
   else
   {
     result=-1;
   }
-  return result;
+  return result; 
 }
+
 void output(int result)
 {
   if(result==1)
@@ -25,9 +28,12 @@ void output(int result)
     printf("the points will not form triangle");
   }
   else
+  {
     printf("the points will form triangle");
+  }
+  
 }
-}
+
 int main()
 {
   float x1,y1,x2,y2,x3,y3,result;
